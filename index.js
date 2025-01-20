8a245a2324fbdbc4c5bd306b61e7be3a12c6d02a
Jasec <jasec_7@hotmail.com>
2025-01-20 02:36:43 -0800

    complete javascript functions continue

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("play-tennis");
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };  
+mondayWork("work from home");
+
+function  wrapAdjective(special = "*"){
+    return function(adjective = "a hard worker"){
+     return `You are ${special}${adjective}${special}!`
+  };
+}    
+
+wrapAdjective("||")("a dedicated programmer");
